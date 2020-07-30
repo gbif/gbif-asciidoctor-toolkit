8ecf3c7834c36523bf743e31ef73ace274291f31
Matthew Blissett <matt@blissett.me.uk>
2020-07-30 14:22:50 +0200

    Tidy up glossary extension.

--- gbif-extensions/lib/glossary.rb
+++ gbif-extensions/lib/glossary.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # glossary-treeprocessor.rb: Apply roles to crossreferences to the glossary.
 #
 # Copyright 2020 Global Biodiversity Information Facility (GBIF)
@@ -24,72 +25,88 @@ end
 class GlossaryTreeprocessor < Extensions::Treeprocessor
   @@replacements = []
 
-  # Look through the glossary block, and extract the anchor identifiers
-  def buildGlossary block
-    block.find_by(context: :dlist).each do |dlist|
+  # Look through the glossary block, and extract any anchor identifiers
+  def find_glossary_anchor_identifiers glossary_block
+    glossary_block.find_by(context: :dlist).each do |dlist|
 
       dlist.items.each do |item|
-        # TODO: multi-term glossaries (more than one word defined).
-        i = item[0][0]
-        # Disable all inline substitutions before reading text.
-        # Is there no better way?
-        s = []
-        s.replace(i.subs)
-        s.map{|ss| i.remove_sub(ss)}
-        text = i.text
         #puts "Glossary definition item #{text}"
-        addG(text)
-        s.map{|ss| i.subs.append(ss)}
-
+        extract_glossary_anchor_identifier(item[0][0].text_source)
       end
     end
   end
 
-  # Add a reference identifier to the list
-  def addG line
-    line.match(/\[\[(.*)(,?.+|)\]\]/) { |m|
-      puts "Added #{m[1]} to glossary #{@@replacements}"
+  # Extract an anchor identifier from a glossary definition list item
+  def extract_glossary_anchor_identifier line
+    # TODO: use InlineAnchorRx
+    line.match(/\[\[([^,]+)(,.+|)\]\]/) { |m|
       mt = m.captures[0]
-      # TODO: How does AsciiDoctor match references?
+      #puts "Anchor “#{mt}” is for a glossary definition."
       @@replacements.append(mt.gsub(/[ _-]/, '[ _-]'))
     }
   end
 
-  # Recurse the document tree, processing any text to find glossary crossreferences.
-  def traverse block
+  # Traverse the document tree, processing any text to find and replace glossary crossreferences.
+  def add_role_to_glossary_crossreferences block
     #puts ""
-    # Check for :macros sub.
-    if defined? block.subs then
-      if block.sub?(:macros) then
-        s = []
-        s.replace(block.subs)
-        s.map{|ss| block.remove_sub(ss)}
 
-        if defined? block.lines then
-          #puts "CONTENT1 #{block.lines}"
-          block.lines = block.lines.map{|l| l = replaceG(l)}
-        end
-        if defined? block.text then
-          text = block.text
-          #puts "CONTENT2 #{block.text}"
-          block.text = replaceG(block.text)
-        end
+    # Description lists contain arrays.
+    unless ::Array === block
+      # Replace title
+      if block.title? then
+        block.title = add_role(block.title_source)
+      end
 
-        s.map{|ss| block.subs.append(ss)}
+      if ::Table::Cell === block then
+        unless block.style == :asciidoc
+          unless block.source.nil? then
+            block.text = add_role(block.source)
+          end
+        end
+      elsif defined? block.subs then
+        # Check for :macros sub, if it is not enabled then crossreferences aren't being parsed anyway.
+        if block.sub?(:macros) then
+          if defined? block.lines_source then
+            block.lines = block.lines_source.map{|l| l = add_role(l)}
+          end
+
+          if defined? block.text_source then
+            unless block.text.nil? then
+              block.text = add_role(block.text_source)
+            end
+          end
+        end
       end
     end
 
     if ::Array === block then
       #puts "#{block[0][0].class}"
-      traverse(block[0][0])
-      #puts "#{block[1].class}"
-      traverse(block[1])
-    else
-      #puts "#{block.id}, #{block.context}"
+      add_role_to_glossary_crossreferences(block[0][0])
 
-      if block.block? and block.content_model == :compound and block.blocks? then
-        block.blocks.each do |b|
-          traverse(b)
+      if defined? block[1] then
+        add_role_to_glossary_crossreferences(block[1])
+      end
+    else
+
+      if block.block? && block.content_model == :compound then
+        if block.blocks? then
+          block.blocks.each do |b|
+            add_role_to_glossary_crossreferences(b)
+          end
+        elsif ::Table === block then
+          block.rows.head.map{|tr|
+            tr.map{|td| add_role_to_glossary_crossreferences(td)}
+          }
+          block.rows.body.map{|tr|
+            tr.map{|td| add_role_to_glossary_crossreferences(td)}
+          }
+          block.rows.foot.map{|tr|
+            tr.map{|td| add_role_to_glossary_crossreferences(td)}
+          }
+        elsif ::Table::Cell === block then
+          if block.style == :asciidoc then
+            add_role_to_glossary_crossreferences(block.inner_document)
+          end
         end
       end
     end
@@ -98,9 +115,12 @@ class GlossaryTreeprocessor < Extensions::Treeprocessor
   # Replace any <<crossreferences>> with a similar crossreference, but with a role.
   # I am surprised not to find a better way to do this, e.g. an object representing
   # the crossreference macro within the line.
-  def replaceG line
-    @@replacements.map {|r|
-      line = line.gsub(/<<(#{r}),?(.*?)>>/i, ' xref:\1[\2,role=glossary]')
+  def add_role line
+    anchor_patterns = "(" + @@replacements.join("|") + ")"
+    line = line.gsub(/<<#{anchor_patterns}(,[^,]+?|)>>/i) {|s|
+      refx = $1
+      reftext = $2.gsub(/^,/, '')
+      "xref:#{refx}[#{reftext},role=glossary]"
     }
     line
   end
@@ -108,13 +128,45 @@ class GlossaryTreeprocessor < Extensions::Treeprocessor
   def process document
     document.blocks.each do |block|
       if block.id == "glossary" then
-        buildGlossary block
+        find_glossary_anchor_identifiers block
       end
     end
 
-    document.blocks.each do |p|
-      traverse(p)
+    unless @@replacements.empty?
+      document.blocks.each do |p|
+        add_role_to_glossary_crossreferences(p)
+      end
     end
+  end
+end
 
+# Rather than disabling inline substitutions before reading text:
+#   s = []
+#   s.replace(block.subs)
+#   s.map{|ss| block.remove_sub(ss)}
+#   block.text
+#   s.map{|ss| block.subs.append(ss)}
+# add methods to get the source text without substitutions applied.
+class AbstractBlock
+  def title_source
+    @title
+  end
+end
+
+class Block
+  def lines_source
+    @lines
+  end
+end
+
+class ListItem
+  def text_source
+    @text
+  end
+end
+
+class Asciidoctor::Table::Cell
+  def source
+    @text
   end
 end
