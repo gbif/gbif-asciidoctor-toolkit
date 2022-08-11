dfa67cff666f45b17cd76dd7743a46c7717f0f8a
Matthew Blissett <matt@blissett.me.uk>
2022-08-11 14:48:58 +0200

    Emoji flag support for Windows.

--- GbifHtmlConverter.rb
+++ GbifHtmlConverter.rb
@@ -115,6 +115,21 @@ module GbifHtmlConverterBase
       end
     end
 
+    # Special flag emoji font for Windows only.
+    # Based on https://github.com/talkjs/country-flag-emoji-polyfill
+    result << %(<script type="text/javascript">
+      if (/windows/i.test(navigator.userAgent)) {
+        const style = document.createElement("style");
+        style.textContent = `@font-face {
+          font-family: "Twemoji Country Flags";
+          unicode-range: U+1F1E6-1F1FF, U+1F3F4, U+E0062-E0063, U+E0065, U+E0067, U+E006C, U+E006E, U+E0073-E0074, U+E0077, U+E007F;
+          src: url('https://cdn.jsdelivr.net/npm/country-flag-emoji-polyfill@0.1/dist/TwemojiCountryFlags.woff2') format('woff2');
+          font-display: swap;
+        }`;
+        document.head.appendChild(style);
+      }
+    </script>)
+
     if node.attr? 'icons', 'font'
       if node.attr? 'iconfont-remote'
         result << %(<link rel="stylesheet" href="#{node.attr 'iconfont-cdn', %[#{cdn_base_url}/font-awesome/#{FONT_AWESOME_VERSION}/css/font-awesome.min.css]}"#{slash}>)
